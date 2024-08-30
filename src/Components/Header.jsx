7ec7585cac812b119938d8f60f81071ecea2e43c
KaungSettThu1873 <168012673+KaungSettThu1873@users.noreply.github.com>
2024-08-30 19:26:16 +0630

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
             </div>
             <hr />
             <a
-              href="viber://add?number=09263430377"
+              href="viber://chat?number=09263430377"
               className="btn btn-primary w-100  mt-lg-3 mt-md-2 mt-sm-1 mt-1"
             >
               Viber Customer Service သို့ ဆက်သွယ်ရန်{" "}
